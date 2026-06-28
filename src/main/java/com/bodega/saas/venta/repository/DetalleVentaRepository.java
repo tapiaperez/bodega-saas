4696e307572edd032f9657539ce119895deee25e
Edgar <edgartapiaperez@gmail.com>
2026-06-28 00:10:49 -0500

    Sprint 3 completado: POS, Ticket, Reportes y Control Automático de Stock

--- src/main/java/com/bodega/saas/venta/repository/DetalleVentaRepository.java
+++ src/main/java/com/bodega/saas/venta/repository/DetalleVentaRepository.java
@@ -3,6 +3,7 @@ package com.bodega.saas.venta.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.bodega.saas.venta.model.DetalleVenta;
 
@@ -10,5 +11,14 @@ public interface DetalleVentaRepository
         extends JpaRepository<DetalleVenta, Long> {
 
     List<DetalleVenta> findByIdVenta(Long idVenta);
+
+    @Query("""
+        SELECT d.idProducto,
+            SUM(d.cantidad)
+        FROM DetalleVenta d
+        GROUP BY d.idProducto
+        ORDER BY SUM(d.cantidad) DESC
+        """)
+        List<Object[]> obtenerProductosMasVendidos();
 
 }
--- src/main/java/com/bodega/saas/venta/repository/VentaRepository.java
+++ src/main/java/com/bodega/saas/venta/repository/VentaRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.bodega.saas.venta.model.Venta;
+import java.util.List;
+
 
 public interface VentaRepository extends JpaRepository<Venta, Long> {
 
@@ -17,5 +19,32 @@ public interface VentaRepository extends JpaRepository<Venta, Long> {
     """)
     Integer obtenerUltimoCorrelativo(
             @Param("serie") String serie);
+
+    @Query("""
+        SELECT COALESCE(SUM(v.total),0)
+        FROM Venta v
+        WHERE DATE(v.fechaEmision)=CURRENT_DATE
+        """)
+        java.math.BigDecimal obtenerVentasHoy();
+
+
+        @Query("""
+        SELECT COALESCE(SUM(v.total),0)
+        FROM Venta v
+        WHERE YEAR(v.fechaEmision)=YEAR(CURRENT_DATE)
+        AND MONTH(v.fechaEmision)=MONTH(CURRENT_DATE)
+        """)
+        java.math.BigDecimal obtenerVentasMes();
+
+
+        @Query("""
+        SELECT COUNT(v)
+        FROM Venta v
+        """)
+        Long contarVentas();
+
+        
+
+        List<Venta> findTop10ByOrderByFechaEmisionDesc();
 
 }
